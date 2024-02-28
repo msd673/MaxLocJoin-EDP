296a3dca16476bc25c8517cc8b972bd219344149
msd673 <2309721596@qq.com>
2024-02-28 10:59:52 +0800

    modify graph.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/*
- * @Author: msd673@hnu.edu.cn
- * @Date: 2022-09-29 23:21:23
- * @LastEditors: KG Lab KG@hnu.edu.cn
- * @LastEditTime: 2023-07-17 13:36:03
- * @FilePath: /data12/home/msd/MaxLocJoin-EDP/main.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #include "graph.cpp"
 #include <ctime>
 clock_t st, ed;
